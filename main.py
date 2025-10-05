525fa15269950ccd7877956be2353bc86ec6bccf
victoralexander132 <alexandervictor550@gmail.com>
2025-10-05 21:05:10 +0000

    Add prompt as parameter instead of hard coded

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
     print("Hello from ai-agent!")
+    if len(sys.argv) < 2:
+        print("Prompt must be provided")
+        sys.exit(1)
+    prompt = sys.argv[1]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model = 'gemini-2.0-flash-001', contents = prompt
     )
 
     print(response.text)
